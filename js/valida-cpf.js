0b4239f659555f11eb21473901bcc89d0cc0fe42
paulomarcelo <paulo.marcelo@gmail.com>
2023-11-02 13:31:29 -0300

    CPF: segundo dígito

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,9 +1,10 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
-    console.log(validaPrimeiroDigito(cpf));
-   // console.log(validaNumerosRepetidos(cpf));
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe");
+    } else {
+        console.log("Esse cpf existe");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -39,4 +40,22 @@ function validaPrimeiroDigito(cpf) {
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
